5bc181513df7508cc3905c129c9d3d7d065835ea
MComScience <m-com3@hotmail.com>
2019-07-04 06:48:22 +0700

    fix bug

--- config/params.php
+++ config/params.php
@@ -33,5 +33,5 @@ return [
     'jwtSecretCode' => $privateKey,
     'icon-framework' => \kartik\icons\Icon::FA,
     'API_TOKEN' => '6615e94372943853d7dad7a3d847440e',
-    'API_BASE_URL' => 'http://10.188.231.11:8081/api'
+    'API_BASE_URL' => 'http://172.16.254.64:8081/api'
 ];
